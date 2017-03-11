6b9ca765c718670e117be96e68f47aa3ec06f1ee
Rahul <rahulsept2191@gmail.com>
2017-03-11 20:48:34 +0530

    added user session to cookie

--- app/services/session.js
+++ app/services/session.js
@@ -1,13 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+	store: Ember.inject.service(),
 	currentUser: null,
 
 	login(user) {
     this.set('currentUser', user);
+    Cookies.set('userId', user.id);
 	},
 
 	logout(user) {
     this.set('currentUser', null);
-	}
+    Cookies.set('userId', null);
+	},
+
+  initializeFromCookie: function(){
+    var userId = Cookies.get('userId');
+    if(!!userId){
+      var user = this.get('store').find('user', userId)
+      this.set('currentUser', user)
+    }
+  }.on('init')
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,6 +9,7 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/js-cookie/src/js.cookie.js');
   
 
   // Use `app.import` to add additional libraries to the generated
